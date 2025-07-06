f24816483e0c036208f38e3512376cbec366eed0
Kwame Mintah <kwame_mintah@hotmail.co.uk>
2025-07-06 16:19:59 +0100

    feat(main): provide option for model to interface with ollama model created
    instead of calling hugging face and being billed, provide option for running ollama model locally and using `litellm` to interface for the agent tasks. requires the user to pull the model before running the application e.g.
    ```shell
    curl http://localhost:11434/api/pull -d "{\"name\": \"Qwen2.5:7B-instruct\"}"
    ```

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
+import logging
 import os
 
+import requests
 from huggingface_hub import login
-from smolagents import load_tool, CodeAgent, InferenceClientModel, DuckDuckGoSearchTool
+from smolagents import (
+    load_tool,
+    CodeAgent,
+    InferenceClientModel,
+    DuckDuckGoSearchTool,
+    LiteLLMModel,
+)
 
 login(token=os.getenv("HUGGING_FACE_TOKEN"), new_session=True)
+logger = logging.getLogger(__name__)
 
+OLLAMA_BASE_API_URL = os.getenv("OLLAMA_BASE_API_URL", "http://localhost:11434")
 
 def playground():
     """
@@ -12,13 +22,22 @@ def playground():
     response.
     :return: Final result
     """
+    if os.getenv("USE_HUGGING_FACE_INTERFACE", False):
+        model = InferenceClientModel(model_id="Qwen/Qwen2.5-72B-Instruct")
+        logger.info("Initialised model via hugging face, will incur costs")
+    else:
+        requests.get(url=OLLAMA_BASE_API_URL).raise_for_status()
+        model = LiteLLMModel(
+            model_id="ollama/Qwen2.5:7B-instruct",
+            api_base=OLLAMA_BASE_API_URL,
+        )
+        logger.info("Initialised model via Ollama, ensure model has been downloaded")
+
     image_generation_tool = load_tool(
         repo_id="m-ric/text-to-image", trust_remote_code=True
     )
 
     search_tool = DuckDuckGoSearchTool()
-
-    model = InferenceClientModel(model_id="Qwen/Qwen2.5-72B-Instruct")
 
     agent = CodeAgent(tools=[image_generation_tool, search_tool], model=model)
 
