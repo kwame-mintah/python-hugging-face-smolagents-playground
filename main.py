ecef2816fc94c3813029cfad3b11442e90f21f75
Kwame Mintah <kwame_mintah@hotmail.co.uk>
2025-07-10 23:10:43 +0100

    refactor(main): use different model for better performance on m1
    changed to `llama3:instruct` instead of `Qwen2.5:72B-instruct`. include more detailed task to attempt to resolve final answer not being called when using ollama

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import logging
 import os
 
 import requests
+from distutils.util import strtobool
 from huggingface_hub import login
 from smolagents import (
     load_tool,
@@ -9,9 +10,12 @@ from smolagents import (
     InferenceClientModel,
     DuckDuckGoSearchTool,
     LiteLLMModel,
+    FinalAnswerTool,
 )
 
-login(token=(os.getenv("HF_TOKEN") or os.getenv("HUGGING_FACE_TOKEN")), new_session=True)
+login(
+    token=(os.getenv("HF_TOKEN") or os.getenv("HUGGING_FACE_TOKEN")), new_session=True
+)
 logger = logging.getLogger(__name__)
 
 OLLAMA_BASE_API_URL = os.getenv("OLLAMA_BASE_API_URL", "http://localhost:11434")
@@ -22,13 +26,13 @@ def playground():
     response.
     :return: Final result
     """
-    if os.getenv("USE_HUGGING_FACE_INTERFACE", False):
+    if strtobool(os.getenv("USE_HUGGING_FACE_INTERFACE", False)):
         model = InferenceClientModel(model_id="Qwen/Qwen2.5-72B-Instruct")
         logger.info("Initialised model via hugging face, will incur costs")
     else:
         requests.get(url=OLLAMA_BASE_API_URL).raise_for_status()
         model = LiteLLMModel(
-            model_id="ollama/Qwen2.5:7B-instruct",
+            model_id="ollama/llama3:instruct",
             api_base=OLLAMA_BASE_API_URL,
         )
         logger.info("Initialised model via Ollama, ensure model has been downloaded")
@@ -38,12 +42,21 @@ def playground():
     )
 
     search_tool = DuckDuckGoSearchTool()
+    final_answer_tool = FinalAnswerTool()
 
-    agent = CodeAgent(tools=[image_generation_tool, search_tool], model=model)
-
-    result = agent.run(
-        task="Find me some restaurants to eat at tonight in london", stream=False
+    agent = CodeAgent(
+        tools=[image_generation_tool, search_tool, final_answer_tool], model=model
     )
+
+    task = """
+    Find me some restaurants to eat at tonight in London.
+    Use web_search to get restaurant data.
+    Summarize your findings using return_final_answer("...").
+    Wrap your Python code in <code>...</code> tags only.
+    End your output after the final answer.
+    """
+
+    result = agent.run(task=task, stream=False)
 
     return result
 
