6de2326bc7ce5b8b767b65820d14080b3372611c
Kwame Mintah <kwame_mintah@hotmail.co.uk>
2025-10-05 22:20:03 +0100

    feat(get_inference_model): add support for configuring gemini inference
    add new environment variables `LLM_PROVIDER`, `GOOGLE_GEMINI_API_KEY` and `GOOGLE_GEMINI_LLM_MODEL`. include gemini as model to be used for smolagent. removed `USE_HUGGING_FACE_INTERFACE` env var and instead provide name of the provider to be used

--- config.py
+++ config.py
@@ -3,6 +3,14 @@ from pydantic_settings import BaseSettings
 
 
 class EnvironmentVariables(BaseSettings):
+    AGENT_TASK: str = Field(
+        description="The task the agent needs to complete",
+        default="Create a hello world app, using FastAPI.",
+    )
+    LLM_PROVIDER: str = Field(
+        description="The inference model provider to use",
+        default="ollama",
+    )
     OLLAMA_BASE_API_URL: str = Field(
         description="The ollama server URL", default="http://localhost:11434"
     )
@@ -10,13 +18,11 @@ class EnvironmentVariables(BaseSettings):
         description="The Ollama model to be used for inference",
         default="ollama/mistral:7b",
     )
-    AGENT_TASK: str = Field(
-        description="The task the agent needs to complete",
-        default="Create a hello world app, using FastAPI.",
+    GOOGLE_GEMINI_API_KEY: str = Field(
+        description="Your Google Gemini API key", alias="GOOGLE_API_KEY", default=""
     )
-    USE_HUGGING_FACE_INTERFACE: bool = Field(
-        description="Whether to make a request to hugging face for model inference",
-        default=False,
+    GOOGLE_GEMINI_LLM_MODEL: str = Field(
+        description="The Gemini model to use", default="gemini-2.5-flash"
     )
     HUGGING_FACE_IMAGE_GENERATION_TOOL: str = Field(
         description="The name of the hugging face repository tool for text to image",
